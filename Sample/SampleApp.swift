fac62b2080b4cfe81e7165aa0fe62617f87b6f09
tony.tran <tranvutuan@hotmail.com>
2021-01-23 20:01:15 -0500

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Tony Tran on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
